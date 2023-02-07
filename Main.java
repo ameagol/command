07fdc96e681600e9e6e99474f7f8005c43f53cae
ricgellman <ricaprado@yahoo.com>
2023-02-07 08:20:42 -0300

    1st commit
    1st commit

---
+++ Main.java
@@ -0,0 +1,41 @@
+import command.Commander;
+
+public class Main {
+    public static void main(String[] args) {
+//        String cloudProvider = "gcp";
+//        String serviceProvider = "vertex";
+//
+//        System.out.println(getCloudData(cloudProvider, serviceProvider));
+
+        Commander c = new Commander();
+        System.out.println(c.getCloudService("azzure", "lambda"));
+
+    }
+
+//    public static String getCloudData(String cloudProvider, String serviceProvider){
+//        if(cloudProvider.equals("aws")){
+//            if(serviceProvider.equals("lambda")){
+//                return "https://docs.aws.amazon.com/lambda";
+//            } else if (serviceProvider.equals("sqs")){
+//                return "https://docs.aws.amazon.com/sqs";
+//            }
+//        }else if(cloudProvider.equals("azzure")){
+//            if(serviceProvider.equals("bot")){
+//                return "https://azure.microsoft.com/en-us/products/bot-services/";
+//            } else if (serviceProvider.equals("bastion")){
+//                return "https://azure.microsoft.com/en-us/services/azure-bastion/";
+//            } else if (serviceProvider.equals("edge")){
+//                return "https://azure.microsoft.com/en-us/products/azure-sql/edge/";
+//            }
+//        }else if(cloudProvider.equals("gcp")){
+//            if(serviceProvider.equals("vertex")){
+//                return "https://cloud.google.com/vertex-ai";
+//            } else if (serviceProvider.equals("dialogflow")){
+//                return "https://cloud.google.com/dialogflow";
+//            } else if (serviceProvider.equals("visionai")){
+//                return "https://cloud.google.com/vision";
+//            }
+//        }
+//        return null;
+//    }
+}
